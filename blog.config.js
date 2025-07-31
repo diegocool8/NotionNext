31de5f05b2e01489f1b83a32cf5e2e4d40d4c837
diegocool8 <117018811+diegocool8@users.noreply.github.com>
2025-07-31 23:33:02 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -15,9 +15,9 @@ const BLOG = {
   APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [20, 6], // 夜间模式起至时间，false时关闭根据时间自动切换夜间模式
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Diego', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '一个沪漂数分人、INFJ、爱理想爱自由', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://www.fanmaimeihaoshenghuo.top/', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '自由，INFJ, 博客,数分', // 网站关键词 英文逗号隔开
+  BIO: process.env.NEXT_PUBLIC_BIO || '从事数据分析的INFJ、读书_运动_音乐让人快乐', // 作者简介
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://www.wangduoduo.top/', // 网站地址
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '读书,运动,看世界,INFJ,数分', // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
   BEI_AN: process.env.NEXT_PUBLIC_BEI_AN || '', // 备案号 闽ICP备XXXXXX
   BEI_AN_LINK: process.env.NEXT_PUBLIC_BEI_AN_LINK || 'https://beian.miit.gov.cn/', // 备案查询链接，如果用了萌备等备案请在这里填写
@@ -62,7 +62,7 @@ const BLOG = {
   // 欢迎语打字效果,Hexo,Matery主题支持, 英文逗号隔开多个欢迎语。
   GREETING_WORDS:
     process.env.NEXT_PUBLIC_GREETING_WORDS ||
-    'Hi，我是一个自由的人, ,Hi，我是一个INFJ,欢迎来到我的博客🎉',
+    'Hi，欢迎来的我的世界',
 
   // uuid重定向至 slug
   UUID_REDIRECT: process.env.UUID_REDIRECT || false
